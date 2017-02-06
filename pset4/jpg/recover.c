d5e2865bb0ac558d3be63518e07f789d1d704d71
Chris Wilson <chris2097@gmail.com>
2017-02-06 19:15:35 +0000

    Pset 4 recover completed

--- pset4/jpg/recover.c
+++ pset4/jpg/recover.c
@@ -34,49 +34,54 @@ int main(int argc, char* argv[])
    
 
     //declare vars and structs
-    typedef uint8_t  BYTE;
-    int filenumber = 000;
-    char* filename = NULL;
-    BYTE block[512];
+    //typedef uint8_t  BYTE;
+    int filenumber = 0;
+    char* filename = (char*) malloc(15);
+    uint8_t block[512];
     FILE* outptr = NULL;
+    bool started = false;
     
     //read block
-    for(int i = 0; i < 512; i++){
-        BYTE temp;
-    block[i] = fread(&temp, sizeof(BYTE), 1, inptr);
-    }
+    //for(int i = 0; i < 512; i++){
+      //  BYTE* temp;
+    //fread(&temp, 1, 512, inptr);
+    
     
     //TODO check if block is .jpg sig
-    if (block[0] == 0xff && block[1] == 0xd8 && block[2] == 0xff && (block[3] == 0xe0 | block[3] == 0xe1 | block[3] == 0xe2 | block[3] == 0xe3 | block[3] == 0xe4 | block[3] == 0xe5 | block[3] == 0xe6 | block[3] == 0xe7 | block[3] == 0xe8 | block[3] == 0xe9 | block[3] == 0xea | block[3] == 0xeb | block[3] == 0xec | block[3] == 0xed | block[3] == 0xee | block[3] == 0xef))
+    while(fread(&block, 1, 512, inptr) == 512)
     {
-        printf("if fired");
-    //if block is .jpg start close file and open new file
-        sprintf(filename, "%03i.jpg", filenumber);
-       if (filename != NULL)
-       {
-           fclose(outptr);
-       }
-    
-    // open output file   
-    outptr = fopen(filename, "w");
-    filenumber++;
-    if (outptr == NULL)
-    {
+        //if block is .jpg start close file and open new file
+        if (block[0] == 0xff && 
+            block[1] == 0xd8 && 
+            block[2] == 0xff && 
+            (block[3] & 0xf0) == 0xe0)
+        {
+            // close old outfile
+            if (filename != NULL && started == true)
+            {
+               fclose(outptr);
+            }
+            started = true;
+            sprintf(filename, "%03i.jpg", filenumber);
+            //increment filenumber
+            filenumber++;
+            // open output file   
+            outptr = fopen(filename, "w");
+            // handle opening error
+            if (outptr == NULL)
+            {
+                fclose(inptr);
+                fprintf(stderr, "Could not create %s.\n", filename);
+                return 3;
+            }
+            fwrite(&block, 1, 512, outptr);
+        // if 
+        }else if (started == true)
+        {
+            fwrite(&block, 1, 512, outptr);    
+        }
+        //TODO close open file
+    }
         fclose(inptr);
-        fprintf(stderr, "Could not create %s.\n", filename);
-        return 3;
-    }
-    fwrite(&block, sizeof(block), 1, outptr);
-    }else if (filename != NULL){
-        printf("else fired");
-    fwrite(&block, sizeof(block), 1, outptr);    
-    }
-    
-    
-    //TODO check if EOF
-    //TODO close open file
-    
+        fclose(outptr);
 }
-
-
- 
